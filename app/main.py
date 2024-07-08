61bb192c5a14ae1d17854c9576a24ae5858ebce9
Daniel <danielantorodri@gmail.com>
2024-07-08 17:48:38 +0530

    update main

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api import users, mutual_fund_data
+from api import users, mutual_fund
 
 app = FastAPI()
 app.include_router(users.router)
-app.include_router(mutual_fund_data.router)
+app.include_router(mutual_fund.router)
 
 origins = [
     "http://localhost:3000",
